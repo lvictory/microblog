092c9c45d485dc90aff1af14dc8112a819e28e3d
lvictory <lxs4242@gmail.com>
2015-03-17 08:24:27 +0800

    finish index page

--- microblog/app.js
+++ microblog/app.js
@@ -5,7 +5,7 @@ var app = module.exports = express.createServer();
 
 app.configure(function(){
     app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
+    app.set('view engine', 'ejs');
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
@@ -22,6 +22,48 @@ app.configure('production', function(){
     app.use(express.errorHandler());
 });
 
+
+
+
 app.get('/', routes.index);
+app.get('/u/:user', routes.user);
+app.get('/post', routes.post);
+app.get('/reg', routes.reg);
+app.post('/reg', routes.doReg);
+app.get('/login', routes.login);
+app.post('/login', routes.doLogin);
+app.get('/logout', routes.logout);
+
+var users = {
+    'lvxinsheng': {
+        name: 'lvinxheng'
+        ,website: 'lvictory.me'
+    }
+}
+
+app.all('/user/:username', function(req, res, next){
+    if(users[req.params.username]){
+        next();
+    } else{
+        next(new Error(req.params.username + 'does not exist'));
+    }
+});
+
+app.get('/user/:username', function(req, res) {
+    res.send(JSON.stringify(users[req.params.username]));
+});
+
+app.put('/user/:username', function(req, res) {
+    res.send('Done');
+});
+
+// app.all('/user/:username', function(req, res, next){
+//     res.send('hello all get');
+//     next();
+// });
+// app.get('/user/:username', function(req, res){
+//     res.send('user: ' + req.params.username);
+// });
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- microblog/routes/index.js
+++ microblog/routes/index.js
@@ -1,3 +1,29 @@
-exports.index = function(req, res) {
-	res.render('index', {title: 'Express'});
-};
+
+var router = {
+	index: function(req, res) {
+		res.render('index', { title: 'Express' }); 
+	}
+	,user: function(req, res) {
+
+	}
+	,post: function(req, res) {
+
+	}
+	,reg: function(req, res) {
+
+	}
+	,doReg: function(req, res) {
+
+	}
+	,login: function(req, res) {
+
+	}
+	,doLogin: function(req, res) {
+
+	}
+	,logout: function(req, res) {
+
+	}
+}
+
+module.exports = router;
